3620be75d01431d25a3cd8750cfbfeb86bdd8717
Jin-woo-rgb <johnsenaris11@gmail.com>
2025-05-01 21:31:40 +0800

    Update index.js

--- includes/database/index.js
+++ includes/database/index.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 const logger = require('../logger');
 
-const uri = "mongodb+srv://Patrick:<isangtite>@raphael.kjzh5cd.mongodb.net/?retryWrites=true&w=majority&appName=Raphael"; // Replace with your MongoDB URL
+const uri = "mongodb+srv://Patrick:isangtite@raphael.kjzh5cd.mongodb.net/?retryWrites=true&w=majority&appName=Raphael"; // Replace with your MongoDB URL
 const client = new MongoClient(uri);
 
 async function connect() {
